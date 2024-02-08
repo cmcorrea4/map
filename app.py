9dcedf1f91806bd114774240f2b72bf85ba27af4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-08 17:09:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import geopandas as gpd
+
+
+# Carga el archivo GeoJSON como un GeoDataFrame
+gdf = gpd.read_file('tu_archivo.geojson')
+
+# Convierte el GeoDataFrame en un DataFrame de pandas
+df2 = pd.DataFrame(gdf)
+
+
+
+
+
+
 df = pd.DataFrame(
     np.random.randn(100, 2) / [50, 50] + [6.250136, -75.566067],  #6.250136 -75.566067
     columns=['lat', 'lon'])
