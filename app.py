37dcd95c521bd2829ff7fad25ee1a4b746f28904
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-09 07:47:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ gdf = gpd.read_file('Mapa de Accidentalidad Vial Municipio de Medellín 2016.geo
 
 # Convierte el GeoDataFrame en un DataFrame de pandas
 df2 = pd.DataFrame(gdf)
-#st.dataframe(df2.head())
+st.write(df.columns.values[1])
 
 
 
