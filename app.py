b82b7bfe6ced019512f4f1583deaeb91181a6d64
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-09 22:26:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,11 +50,8 @@ df2['geometry'].shape
 #st.dataframe(df4)
 
 
-
-
-
 df = pd.DataFrame(
-    np.random.randn(100, 2) / [50, 50] + [6.250136, -75.566067],  #6.250136 -75.566067
+    np.random.randn(10, 2) / [50, 50] + [6.250136, -75.566067],  #6.250136 -75.566067
     columns=['lat', 'lon'])
 
 st.map(df)
