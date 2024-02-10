c953e57ae0f69c4952f0075c345054a6819ccab3
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-09 22:03:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ df4 = pd.DataFrame({'Latitud':lat[0 : 10]})
 
 #df4 = pd.DataFrame(lat, columns=['latitud'])
 
-st.dataframe(df4)
+st.write(df4)
 
 
 # Convierte el GeoDataFrame en un DataFrame de pandas
