06404593ca95631ca106464b19ee25fe47af3e26
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-12 15:31:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ option_hour_min = st.selectbox('Selecciona filtro por Hora',
                                ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='1')
 option_hour_max = st.selectbox('Selecciona filtro por Hora',
                                ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='2')
-option_day = st.selectbox('Selecciona filtro por día',('LUNES', 'MARTES', 'MIÉRCOLES','JUEVES','VIERNES'))
+option_day = st.selectbox('Selecciona filtro por día',('LUNES', 'MARTES', 'MIÉRCOLES','JUEVES','VIERNES','SÁBADO','DOMINGO'))
 #df_filtrado = df_g.query('día == "MIÉRCOLES" and Hora >= "08:00:00" and Hora <= "10:00:00"')
 df_filtrado = df_g.query('día == @option_day and Hora >=  @option_hour_min ')
 st.write(df_filtrado)
