2000ad170b1e1bbb02b38d87cf5d03a9f166c05a
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-12 17:09:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ st.title("Accidentalidad Municipio de Medellín 2016")
 # Carga el archivo GeoJSON como un GeoDataFrame
 #gdf = gpd.read_file('Mapa de Accidentalidad Vial Municipio de Medellín 2016.geojson')
 
-st.write("Se entiende por accidente de tránsito  evento, generalmente involuntario, generado al menos por un un vehículo en movimiento, que causa daños a 
-         personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el 
-         lugar o dentro de la zona de influencia del hecho0 (Ley 769 de 2002 - Código Nacional de Tránsito"
+st.write('Se entiende por accidente de tránsito  evento, generalmente involuntario, generado al menos por un un vehículo en movimiento, que causa daños a '
+         'personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el' 
+         'lugar o dentro de la zona de influencia del hecho0 (Ley 769 de 2002 - Código Nacional de Tránsito)'
          )
 
 
