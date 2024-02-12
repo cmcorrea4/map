56d11017c20daac8bad84781a0f1dc6561e236b9
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-02-12 10:56:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ df = pd.DataFrame(
     np.random.randn(10, 2) / [50, 50] + [6.250136, -75.566067],  #6.250136 -75.566067
     columns=['lat', 'lon'])
 
-st.map(df)
-st.write(df)
+st.map(df_filtrado)
+#st.write(df)
